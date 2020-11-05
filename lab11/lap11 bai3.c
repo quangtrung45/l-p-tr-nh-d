877cb8d3fc6bec1ff9aefe3f6f52bd86d5fbab3f
quangtrung45 <73331411+quangtrung45@users.noreply.github.com>
2020-11-05 23:19:22 +0700

    Add files via upload

---
+++ lab11/lap11 bai3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char num[5];
+	int i, j;
+	
+	printf("Nhap 5 so bat ky");
+	gets(num[5]);
+	
+	j=num[5];
+	int reverse;
+	printf("\n 5 so nguoc lai la:%s ",num[5]);
+
+	
+}
+ void reverse(int num[5])
+ {
+ 	char i,j;
+ 	i = num[5];
+ 	num[i]=num[j-i];
+ 	num[j-i]=i;
+ 	
+ 	if(i==j)
+ 	{
+ 		return;
+	 }
+ 	reverse;
+ 	
+ }
+ 
+ 
+ 
+ 

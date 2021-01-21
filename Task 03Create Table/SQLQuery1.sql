8bcfbf9a8c0c27f66ec7023e544762e9469f30bb
quangtrung45 <73331411+quangtrung45@users.noreply.github.com>
2021-01-21 21:36:24 +0700

    Add files via upload

---
+++ Task 03Create Table/SQLQuery1.sql
@@ -0,0 +1,77 @@
+﻿IF EXISTS (SELECT * FROM sys.databases WHERE NAME LIKE 'Example5')
+DROP DATABASE Example5
+
+
+CREATE DATABASE Example5
+GO
+
+USE Example5
+GO
+
+-- TẠO BẢNG lớp học
+CREATE TABLE LopHoc(
+MaLopHoc INT PRIMARY KEY IDENTITY,
+TenLopHoc VARCHAR(10)
+)
+
+GO
+INSERT INTO LopHoc(TenLopHoc) VALUES ('T1801A');
+INSERT INTO LopHoc(TenLopHoc) VALUES ('T1908A');
+INSERT INTO LopHoc(TenLopHoc) VALUES ('T1909A');
+INSERT INTO LopHoc(TenLopHoc) VALUES ('T2009E');
+GO
+SELECT * FROM LopHoc
+GO
+
+-- tạo bảng sinh viên có khóa ngoại là cột MaLopHoc,nối với bảng lớp
+CREATE TABLE SinhVien(
+MaSV int PRIMARY KEY,
+TenSV nvarchar(40),
+MaLopHoc int,
+CONSTRAINT fk FOREIGN KEY (MaLopHoc) REFERENCES LopHoc(MaLopHoc)
+)
+GO
+
+
+
+--thu insert data (đúng,sai)
+-- Bài tập:
+--tạo 2 bảng:
+--category(id,name)
+--product(id,name,price,catid)
+
+CREATE Table category( 
+Id int PRIMARY KEY,
+Name nvarchar(50),
+)
+GO
+SELECT *From Category
+GO
+
+INSERT INTO Category(Name,Id) VALUES ('Hoang',1187);
+INSERT INTO Category(Name,Id) VALUES ('Hoang2',1190);
+INSERT INTO Category(Name,Id) VALUES ('Hoang3',1160);
+INSERT INTO Category(Name,Id) VALUES ('Hoang4',1141);
+
+CREATE TABLE Product (
+Id int PRIMARY KEY,
+Name nvarchar(50),
+Price int,
+Catid int,
+CONSTRAINT fk FOREIGN KEY(catid) REFERENCES Category(Id)
+)
+GO
+
+SELECT *From Product
+GO
+INSERT INTO Product(Name,Id,price,catid) VALUES ('xiaomi',10,1187);
+INSERT INTO Product(Name,Id,price,catid) VALUES ('T2005S',15,1190);
+INSERT INTO Product(Name,Id,price,catid) VALUES ('T2001C',19,1160);
+INSERT INTO Product(Name,Id,price,catid) VALUES ('T2008Q',25,1141);
+GO
+
+
+
+
+
+
---
+++ Task 03Create Table/SQLQuery3.sql
@@ -0,0 +1,32 @@
+﻿IF EXISTS (SELECT * FROM sys.databases WHERE Name='Example5')
+DROP DATABASE Example5
+GO
+CREATE DATABASE Example5
+GO
+USE Example5
+GO
+--Tạo bảng Lớp học
+CREATE TABLE LopHoc(
+MaLopHoc INT PRIMARY KEY IDENTITY,
+TenLopHoc VARCHAR(10)
+)
+GO
+--Tạo bảng Sinh viên có khóa ngoại là cột MaLopHoc, nối với bảng
+LopHoc
+CREATE TABLE SinhVien(
+MaSV int PRIMARY KEY,
+TenSV varchar(40),
+MaLopHoc int,
+CONSTRAINT fk FOREIGN KEY (MaLopHoc) REFERENCES LopHoc(MaLopHoc)
+)
+GO
+--Tạo bảng SanPham với một cột NULL, một cột NOT NULL
+CREATE TABLE SanPham (
+MaSP int NOT NULL,
+TénP varchar(40) NULL
+)
+GO
+--Tạo bảng với thuộc tính default cho cột Price
+CREATE TABLE StoreProduct(
+ProductID int NOT NULL,
+Name varchar(40) NOT NULL,

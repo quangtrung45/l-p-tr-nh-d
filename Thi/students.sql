f3baa9ee971860a020fb7970dfe1f893c96e4654
quangtrung45 <73331411+quangtrung45@users.noreply.github.com>
2021-05-21 14:51:28 +0700

    Add files via upload

---
+++ Thi/students.sql
@@ -0,0 +1,7 @@
+create table students_school(
+StudentId varchar(50),
+StudentName varchar(50),
+Adrees varchar(50),
+Phone int
+);
+insert into students_school values('T2010A','Bui Van hoa','Ha Noi',0376306599);
